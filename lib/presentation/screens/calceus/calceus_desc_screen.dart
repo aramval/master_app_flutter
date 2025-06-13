2278c452697fcfc317dcd46c6197171d641e2d2a
AnnaRamosV <anna212192@gmail.com>
2025-06-13 21:16:18 +0200

    clase 50

--- lib/presentation/screens/calceus/calceus_desc_screen.dart
+++ lib/presentation/screens/calceus/calceus_desc_screen.dart
@@ -31,10 +31,6 @@ class CalceusDescScreen extends StatelessWidget {
           )
             ],
           ),
-           
-           
-           
-           
            Expanded(
               child: SingleChildScrollView(
                  child: Column(
@@ -45,7 +41,9 @@ class CalceusDescScreen extends StatelessWidget {
             'Nike Air Maz 720', 
             descriptio: "The Nike Air Max 720 goes bigger than ever before with Nike's taller Air unit yet, offering more air underfoot for unimaginable, all-day comfort. Has Air Max gone too far? We hope so."
             ),
-            _PrettiumEtBuyNow()
+            _PrettiumEtBuyNow(),
+            _ColoresEtAlterButton(),
+            _ButtonsLikeCartSettings()
               ],
             ),
               )
@@ -77,6 +75,109 @@ class _PrettiumEtBuyNow extends StatelessWidget {
             
           ],),
       ),
+    );
+  }
+}
+
+class _ColoresEtAlterButton extends StatelessWidget {
+  const _ColoresEtAlterButton();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
+      child: Row(
+        children: [
+          Expanded(child: Stack(
+            children: [
+              Positioned(
+                left: 90,
+                child:   const _ActioButtonColor(color: Color.fromARGB(255, 198, 214, 66)),
+                ),
+                  Positioned(
+                left: 60,
+                child:   const _ActioButtonColor(color: Color.fromARGB(255, 255, 173, 41)),
+                ),
+                  Positioned(
+                left: 30,
+                child:      const _ActioButtonColor(color: Color.fromARGB(255, 32, 153, 241)),
+                ),
+                 _ActioButtonColor(color: Color.fromARGB(255, 54, 77, 86)),
+            ],
+          )),
+
+
+        
+            
+          
+            
+
+
+                const ButtonAurantius(textus: 'More related items',
+                altus: 30,
+                latus: 170,
+                color: Color.fromARGB(255, 255, 198, 17),)
+        ],
+      ),
+    );
+  }
+}
+
+class _ActioButtonColor extends StatelessWidget {
+  final Color color;
+  const _ActioButtonColor({required this.color});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 45,
+      height: 45,
+      decoration: BoxDecoration(color: color, shape: BoxShape.circle),
+    );
+  }
+}
+
+
+class _ButtonsLikeCartSettings extends StatelessWidget {
+  const _ButtonsLikeCartSettings();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 30),
+      padding: const EdgeInsets.symmetric(horizontal: 30),
+      child: Row(
+        children: [
+          _ButtonCumUmbra(icon: Icon(Icons.star, color: Colors.red, size: 25)),
+          _ButtonCumUmbra(icon: Icon(Icons.add_shopping_cart, color: Colors.grey.withValues(alpha: 0.4), size: 25)),
+          _ButtonCumUmbra(icon: Icon(Icons.settings, color: Colors.grey.withValues(alpha: 0.4), size: 25)),
+        ]
+      ),
+    );
+  }
+}
+
+class _ButtonCumUmbra extends StatelessWidget {
+  
+  final Icon icon;
+
+  const _ButtonCumUmbra({
+    required this.icon
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 55,
+      height: 55,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        shape: BoxShape.circle,
+        boxShadow: const[
+          BoxShadow(color: Colors.black12, spreadRadius: -5, blurRadius: 20, offset: Offset(0, 10))
+        ]
+      ),
+      child: icon,
     );
   }
 }
